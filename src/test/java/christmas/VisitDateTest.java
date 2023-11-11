1f9046949f54a0b9cd8ee371d7d4fce7aeb9898d
chaniii999 <psc0729@naver.com>
2023-11-11 16:32:51 +0900

    test : 방문날짜 기능 테스트

---
+++ src/test/java/christmas/VisitDateTest.java
@@ -0,0 +1,45 @@
+package christmas;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static christmas.SystemMessage.OutputViewMessage.ERROR_DATE;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class VisitDateTest extends ApplicationTest {
+
+
+
+    @Test
+    void overRangeThrowTest() {
+        assertSimpleTest(() -> {
+            runException("32");
+            assertThat(output()).contains(ERROR_DATE.getForm());
+        });
+    }
+
+    @Test
+    void zeroThrowTest() {
+        assertSimpleTest(() -> {
+            runException("0");
+            assertThat(output()).contains(ERROR_DATE.getForm());
+        });
+    }
+
+    @Test
+    void underRangeThrowTest() {
+        assertSimpleTest(() -> {
+            runException("-5");
+            assertThat(output()).contains(ERROR_DATE.getForm());
+        });
+    }
+
+    @Test
+    void inValidThrowTest() {
+        assertSimpleTest(() -> {
+            runException("31k");
+            assertThat(output()).contains(ERROR_DATE.getForm());
+        });
+    }
+
+}
