c23e34c2d6e935b2319ff47880ed285cbf6833f2
chaniii999 <psc0729@naver.com>
2023-11-13 15:06:16 +0900

    feat : 주문가능리스트, 메뉴 클래스 구현

---
+++ src/main/java/christmas/service/Menu.java
@@ -0,0 +1,28 @@
+package christmas.service;
+
+public class Menu {
+    private final String name;
+    private final int price;
+    private final String type;
+
+    public Menu(String name,int price,String type) {
+        this.name = name;
+        this.price = price;
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+
+
+}
