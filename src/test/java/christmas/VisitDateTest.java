6572ba6490d9c65dbee364dcf6912e61b0e61eb9
chaniii999 <psc0729@naver.com>
2023-11-13 15:05:53 +0900

    test : 예약날짜 예외처리 테스트 추가

--- src/test/java/christmas/VisitDateTest.java
+++ src/test/java/christmas/VisitDateTest.java
@@ -1,7 +1,7 @@
 package christmas;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
-import static christmas.SystemMessage.OutputViewMessage.ERROR_DATE;
+import static christmas.OutputView.OutputViewMessage.ERROR_DATE;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
@@ -41,5 +41,6 @@ public class VisitDateTest extends ApplicationTest {
             assertThat(output()).contains(ERROR_DATE.getForm());
         });
     }
+
 
 }
