ffced1bd3551c7adddd6f2852374d8b3de9a341f
chaniii999 <psc0729@naver.com>
2023-11-13 15:07:02 +0900

    test : 주문 입력 기능 테스트

---
+++ src/test/java/christmas/TakeOrderTest.java
@@ -0,0 +1,63 @@
+package christmas;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static christmas.OutputView.OutputViewMessage.ERROR_ONLY_DRINK;
+import static christmas.OutputView.OutputViewMessage.ERROR_ORDER;
+import static christmas.OutputView.OutputViewMessage.ERROR_OVER_COUNT;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class TakeOrderTest extends ApplicationTest{
+
+    @Test
+    void inValidFormatThrowTest() {
+        assertSimpleTest(() -> {
+            runException("31","레드와인+1,티본스테이크-1");
+            assertThat(output()).contains(ERROR_ORDER.getForm());
+        });
+    }
+
+    @Test
+    void inValidThrowTest() {
+        assertSimpleTest(() -> {
+            runException("31","레드와인 - 2");
+            assertThat(output()).contains(ERROR_ORDER.getForm());
+        });
+    }
+
+    @Test
+    void oneDrinkThrowTest() {
+        assertSimpleTest(() -> {
+            runException("31","레드와인-1");
+            assertThat(output()).contains(ERROR_ONLY_DRINK.getForm());
+        });
+    }
+
+    @Test
+    void duplicateThrowTest() {
+        assertSimpleTest(() -> {
+            runException("31","레드와인-1,레드와인-2");
+            assertThat(output()).contains(ERROR_ORDER.getForm());
+        });
+    }
+
+    @Test
+    void onlyDrinkThrowTest() {
+        assertSimpleTest(() -> {
+            runException("31","레드와인-1,제로콜라-2");
+            assertThat(output()).contains(ERROR_ONLY_DRINK.getForm());
+        });
+    }
+
+
+
+    @Test
+    void overCountThrowTest() {
+        assertSimpleTest(() -> {
+            runException("31","레드와인-21");
+            assertThat(output()).contains(ERROR_OVER_COUNT.getForm());
+        });
+    }
+
+}
